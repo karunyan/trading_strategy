de33bf1d2864ef3064efa514cb4bcfdb3ba1ba2e
Karunyan Krishnasamy <karun.krishna@gmail.com>
2013-02-04 21:42:49 -0500

    SMA Test Class

---
+++ Indicators/src/SMA_Test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Karunyan Krishnasamy
+ *
+ */
+public class SMA_Test {
+
+	/**
+	 * @param This class is to test the SMA Implementation
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
