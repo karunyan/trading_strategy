a1f757d690a12e25f331bb2ecb730f62ce3638ec
Karunyan Krishnasamy <karun.krishna@gmail.com>
2013-02-04 22:32:04 -0500

    SMA Implementation and Example

--- Indicators/src/SMA_Test.java
+++ Indicators/src/SMA_Test.java
@@ -4,16 +4,23 @@
 
 /**
  * @author Karunyan Krishnasamy
- *
+ * 
  */
 public class SMA_Test {
 
 	/**
-	 * @param This class is to test the SMA Implementation
+	 * @param This
+	 *            class is to test the SMA Implementation
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int[] PriceList = { 1, 2, 4, 3, 6, 8, 1, 1, 4 };
 
+//		for (int i = 0; i < PriceList.length; i++) {
+//			System.out.print(PriceList[i] + " ");
+//
+//		}
+		
+		
 	}
-
 }
